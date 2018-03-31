6ba435e4389d98c46362df697370c04b9ef3c462
brunobc <brunobezerrachaves@gmail.com>
2018-03-31 19:19:14 -0300

    spread operator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
 console.log('Hello TypeScript!');
+
+const toppings = ['bacon', 'more bacon'];
+const newToppings = ['pepperoni with bacon'];
+const all = [...toppings, ...newToppings];
+console.log(all);
