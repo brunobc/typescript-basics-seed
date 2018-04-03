c22a84d9eb92adaa86f3d41c4b4c9e25b727dbfa
brunobc <brunobezerrachaves@gmail.com>
2018-04-02 21:26:19 -0300

    overloads

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+function reverse(str: string): string;
+function reverse<T>(arr: T[]): T[];
+function reverse<T>(stringOrArray: string | T[]): string | T[] {
+    if (typeof stringOrArray === 'string') {
+        return stringOrArray
+            .split('')
+            .reverse()
+            .join('');
+    }
+    return stringOrArray.slice().reverse();
+}
+
+reverse('Pepperoni');
+reverse(['bacon', 'pepperoni', 1]);
+
+
+
+
+
 //let  sumOrder: Function;
 let  sumOrder: (price: number, quantity: number) => number;
 // let  sumOrder: (price: number, quantity: number) => number = (x, y) => x * y;
